1424e2231c691d3ad7cd59a7129d4de1c260c712
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-10 16:00:44 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ window.hexvalues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", 
 function createNew10x10() {
 	var row;
 	var column;
-	for (row = 0; row <= 10; row++) {
-		for (column = 0; column <= 10; column++) {
+	for (row = 0; row <= 9; row++) {
+		for (column = 0; column <= 9; column++) {
 			var id = "T1R" + (row + 1) + "C" + (column + 1);
 			document.getElementById(id).attributes[1].value = "background-color: #" + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)];
 		}
