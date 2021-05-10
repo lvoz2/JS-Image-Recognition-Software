5c936300c5584659583ae075dc20b98b80dfe8e1
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-10 15:56:09 +1000

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.hexvalues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+function createNew10x10() {
+	var row;
+	var column;
+	for (row = 0; row <= 10; row++) {
+		for (column = 0; column <= 10; row++) {
+			var id = "T1R" + (row + 1) + "C" + (column + 1);
+			document.getElementById(id).attributes[1].value = "background-color: #" + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)];
+		}
+	}
+}
