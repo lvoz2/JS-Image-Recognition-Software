6b6f491ba4da1170205f6e834ec6bfed24327436
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-10 15:58:41 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function createNew10x10() {
 	var row;
 	var column;
 	for (row = 0; row <= 10; row++) {
-		for (column = 0; column <= 10; row++) {
+		for (column = 0; column <= 10; column++) {
 			var id = "T1R" + (row + 1) + "C" + (column + 1);
 			document.getElementById(id).attributes[1].value = "background-color: #" + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)] + hexvalues[Math.floor(Math.random() * 16)];
 		}
