f5b86b291d9b2e22c47628619c092aa79d3e7215
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-17 16:42:50 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ window.hexvalues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", 
 window.a;
 window.y = [];
 window.x = [];
+window.tlx = [];
+window.tly = [];
+window.outputx = [];
+window.outputy = [];
 window.zerox = [0,+1,+2,0,+2,0,+2,0,+1,+2];
 window.onex = [0,+1,+1,+1,0,+1,+2];
 window.twox = [0,+1,+2,+2,0,+1,+2,0,0,+1,+2];
@@ -56,4 +60,5 @@ function idofui() {
 			}
 		}
 	}
+	
 }
