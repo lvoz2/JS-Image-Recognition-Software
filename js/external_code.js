395f1f223ba9076744ef72a0fe34e2614fe676db
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-24 12:57:12 +1000

    Update external_code.js

--- js/external_code.js
+++ js/external_code.js
@@ -1,1 +1,22 @@
-// This is the location for external js code 
+// This is the location for external js code that was taken from external sources and wasn't developed by me
+var openFile = function(file) {
+	window.input = file.target;
+	var reader = new FileReader();
+	reader.onload = function(){
+		var dataURL = reader.result;
+		var output = document.getElementById('user_image');
+		output.src = dataURL;
+		window.width = document.getElementById("user_image").width;
+		window.height = document.getElementById("user_image").height;
+	};
+	reader.readAsDataURL(input.files[0]);
+};
+function getImageData() {
+	var c = document.getElementById("user_canvas");
+	var ctx = c.getContext("2d");
+	var img = document.getElementById("user_image");
+	c.width = width;
+	c.height = height;
+	ctx.drawImage(img, 0, 0);
+	window.imgData = ctx.getImageData(0, 0, c.width, c.height);
+}
