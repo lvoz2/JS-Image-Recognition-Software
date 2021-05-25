3b0acdf9cbae3acd97a9ceba7f4ed36c010c2b28
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-05-25 13:14:21 +1000

    Update external_code.js

--- js/external_code.js
+++ js/external_code.js
@@ -8,16 +8,16 @@ var openFile = function(file) {
 		output.onload = function() {
 			window.width = this.width;
 			window.height = this.height;
+			var c = document.getElementById("user_canvas");
+			var ctx = c.getContext("2d");
+			var img = document.getElementById("user_image");
+			c.width = width;
+			c.height = height;
+			ctx.drawImage(img, 0, 0);
+			window.imgData = ctx.getImageData(0, 0, c.width, c.height);
+			convertImgData()
 		};
 		output.src = dataURL;
 	};
 	reader.readAsDataURL(input.files[0]);
-	var c = document.getElementById("user_canvas");
-	var ctx = c.getContext("2d");
-	var img = document.getElementById("user_image");
-	c.width = width;
-	c.height = height;
-	ctx.drawImage(img, 0, 0);
-	window.imgData = ctx.getImageData(0, 0, c.width, c.height);
-	convertImgData()
 }
