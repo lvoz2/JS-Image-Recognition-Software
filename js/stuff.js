57b5c16aca977cdd979ef17b2c02c52b3dc7e941
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-08 11:07:39 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -1,2 +1,2 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-window.octokit = new Octokit({ auth: `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`});
+window.octokit = new Octokit({ auth: `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`});
