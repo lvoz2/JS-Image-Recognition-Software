1b8f70fef247fdb0527626f909d13ed3c5a501c9
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-15 10:57:50 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -1,2 +1,6 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-window.octokit = new Octokit({ auth: `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`});
+window.octokit = new Octokit({
+  auth: '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',
+  userAgent: 'lvoz2',
+  timeZone: 'Australia/Sydney',
+});
