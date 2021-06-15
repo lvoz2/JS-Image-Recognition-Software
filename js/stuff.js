36a78d0ae2b996d44ef312735b15dcc7594b908d
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-15 16:05:14 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -1,6 +1,14 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-window.octokit = new Octokit({
-  auth: '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',
-  userAgent: 'lvoz2',
-  timeZone: 'Australia/Sydney',
+const octokit = new Octokit({
+	auth: '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',
+	userAgent: 'lvoz2',
+	timeZone: 'Australia/Sydney',
 });
+async function sendNewData() {
+	await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+		owner: 'lvoz2',
+		repo: 'JS-Image-Recognition-Software',
+		path: 'text.txt',
+		message: 'Add new data'
+	})
+}
