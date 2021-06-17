ffe39a5c38b75e1e144bec1fc8749a22b357e245
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-18 09:59:19 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -1,7 +1,6 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 import { createOAuthAppAuth } from "https://cdn.skypack.dev/@octokit/auth-oauth-app";
-const { OAuthApp, createNodeMiddleware } = require("@octokit/oauth-app");
-
+import { OAuthApp, createNodeMiddleware } from "https://cdn.skypack.dev/@octokit/oauth-app";
 const app = new OAuthApp({
   clientType: "oauth-app",
   clientId: "7415eb3be51e7222a91c",
