78c96cdaec5f69515a0341c2d5358b0ee74c726a
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-29 17:58:43 +1000

    Update and rename _stream_wrap.js to -stream-wrap.js

--- node-modules/-stream-wrap.js
+++ node-modules/-stream-wrap.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = require('internal/js_stream_socket');
+module.exports = require('/JS-Image-Recognition-Software/node-modules/internal/js-stream-socket');
 process.emitWarning('The _stream_wrap module is deprecated.',
                     'DeprecationWarning', 'DEP0125');
