bbe89abd40f8e10fb31c11fe9d262d6042cb9a06
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-30 12:36:42 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -1,7 +1,7 @@
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 import { createOAuthAppAuth } from "https://cdn.skypack.dev/@octokit/auth-oauth-app";
 import { OAuthApp, createNodeMiddleware } from "https://cdn.skypack.dev/@octokit/oauth-app";
-var http = Tarp.require({main: "https://lvoz2.github.io/JS-Image-Recognition-Software/http.js"});
+window.http = Tarp.require({main: "https://lvoz2.github.io/JS-Image-Recognition-Software/http.js"});
 window.authenticate = function() {
 	const app = new OAuthApp({
 	  clientType: "oauth-app",
