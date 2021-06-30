3cdd081ea9553e02d85a0badeeb3e01182fbcd60
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-06-30 10:56:25 +1000

    Update promises.js

--- node-modules/dns/promises.js
+++ node-modules/dns/promises.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const dnsPromises = require('internal/dns/promises');
-dnsPromises.setServers = require('dns').setServers;
+const dnsPromises = require('/JS-Image-Recognition-Software/node-modules/internal/dns/promises');
+dnsPromises.setServers = require('/JS-Image-Recognition-Software/node-modules/dns').setServers;
 module.exports = dnsPromises;
