34d854bef0b672804a8aaf0020a309ba56df930f
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-06 12:08:59 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -1,57 +1,23 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 import { createOAuthAppAuth } from "https://cdn.skypack.dev/@octokit/auth-oauth-app";
-import { OAuthApp, createNodeMiddleware } from "https://cdn.skypack.dev/@octokit/oauth-app";
-window.http = Tarp.require({main: "https://lvoz2.github.io/JS-Image-Recognition-Software/http.js"});
-window.authenticate = function() {
-	const app = new OAuthApp({
-	  clientType: "oauth-app",
-	  clientId: "7415eb3be51e7222a91c",
-	  clientSecret: "f43a1da2796648bb8f8d98a166ff3278d7843624"
+window.authenticate = async function() {
+	window.OAuth = createOAuthAppAuth({
+		clientType: "oauth-app",
+		clientId: "7415eb3be51e7222a91c",
+		clientSecret: "f43a1da2796648bb8f8d98a166ff3278d7843624"
 	});
-
-	app.on("token", async ({ token, octokit }) => {
-	  const { data } = await octokit.request("GET /user");
-	  console.log(`Token retrieved for ${data.login}`);
+	window.userAuthenticationFromWebFlow = await OAuth({
+		type: "oauth-user",
+		code: "random123",
+		state: "mystate123"
 	});
-	http.createServer(createNodeMiddleware(app)).listen(3000);
+	window.octokit = new Octokit({
+		authStrategy: createOAuthAppAuth,
+		auth: userAuthenticationFromWebFlow
+	});
 }
-// can now receive user authorization callbacks at /api/github/oauth/callback
-// See all endpoints at https://github.com/octokit/oauth-app.js#middlewares
-/*
-var auth = createOAuthAppAuth({
-	clientType: "oauth-app",
-	clientId: "7415eb3be51e7222a91c",
-	clientSecret: "f43a1da2796648bb8f8d98a166ff3278d7843624"
-});
-var userAuthenticationFromWebFlow = await auth({
-	type: "oauth-user",
-	code: "random123",
-	state: "mystate123"
-});
-const octokit = new Octokit({
-	authStrategy: createAppAuth,
-	auth: userAuthenticationFromWebFlow
-});
-*/
-window.octokit = new Octokit({
-	clientType: "oauth-app",
-	clientId: "7415eb3be51e7222a91c",
-	clientSecret: "f43a1da2796648bb8f8d98a166ff3278d7843624",
-	userAgent: 'JS-Image-Recognition-Software'
-});
 window.sendNewData = async function() {
-	/* var ghCode = await octokit.request('GET https://github.com/login/oauth/authorize', {
-		client_id: '7415eb3be51e7222a91c',
-		scopes: 'repo',
-		Origin: 'https://lvoz2.github.io'
-	})
-	await octokit.request('GET https://github.com/login/oauth/authorize', {
-		client_id: '7415eb3be51e7222a91c',
-		client_secret: 'f43a1da2796648bb8f8d98a166ff3278d7843624',
-		code: ghCode
-	})
-	*/
-	await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+	octokit.rest.repos.createOrUpdateFileContents({
 		owner: 'lvoz2',
 		repo: 'JS-Image-Recognition-Software',
 		path: 'text.txt',
