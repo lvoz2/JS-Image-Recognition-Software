a35954e3e0e1d39ea1cb559156684448d8160bbf
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-07 10:19:51 +1000

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,15 +1,13 @@
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+import { OAuthApp, createNodeMiddleware } from "https://cdn.skypack.dev/@octokit/oauth-app";
+window.http = require('http');
+const app = new OAuthApp({
+  clientType: "oauth-app",
+  clientId: "7415eb3be51e7222a91c",
+  clientSecret: "f43a1da2796648bb8f8d98a166ff3278d7843624"
 });
 
-server.listen(() => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.on("token", async ({ token, octokit }) => {
+  const { data } = await octokit.request("GET /user");
+  console.log(`Token retrieved for ${data.login}`);
 });
+http.createServer(createNodeMiddleware(app)).listen(3000);
