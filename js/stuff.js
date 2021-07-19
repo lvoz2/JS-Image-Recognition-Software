5487ff5343a5eda21d1bcd854dd1752a688125ee
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-19 13:17:49 +1000

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -16,9 +16,9 @@ window.windowAuth = function() {
 		var codelocation = url.indexOf("=");
 		var code = url.slice(codelocation + 1);
 		popup.close()
-		$.get('https://staging.tm1explorers.com/token.php?code=' + code, function (access_token) {
+		$.get('https://api.allorigins.win/get?url=' + encodeURIComponent('https://github.com/login/oauth/access_token?client_id=7415eb3be51e7222a91c&client_secret=f43a1da2796648bb8f8d98a166ff3278d7843624&code=') + code, function (access_token) {
 			// Step 7
-			$.getJSON('https://api.github.com/user?access_token=' + access_token, function (user) {
+			$.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://api.github.com/user?access_token=' + access_token), function (user) {
 				console.log(user.login);
 			});
 		});
