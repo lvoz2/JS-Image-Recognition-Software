7ed743e729cb0f2cad5075fe0b018f9445c90384
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-20 09:48:00 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		console.log(event));
+	event.respondWith(function() {
+		console.log(event);
 		console.log(event.request.url);
 		var url = encodeURIComponent(event.request.url);
 		event.request.url = "https://api.allorigins.win/get?url=" + url;
 		console.log(event.request.url);
 		fetch(event.request);
-	);
+	});
 });
 
