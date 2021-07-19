80aca14b4b4868775c23947f0c1d298c1c191a86
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-20 09:11:11 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 self.addEventListener('fetch', function(event) {
   console.log(event);
+  console.log(event.request.url);
   var url = encodeURIComponent(event.request.url);
   event.request.url = "https://api.allorigins.win/get?url=" + url;
+  console.log(event.request.url);
   event.respondWith(
     fetch(event.request)
   );
