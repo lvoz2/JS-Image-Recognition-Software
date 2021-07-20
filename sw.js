3443a4a40af59d3193ee2ee087faec306844195b
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-20 10:08:22 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
 self.addEventListener('fetch', function(event) {
-	console.log(event);
-	console.log(event.request.url);
-	var origin = event.request.url.slice(0, event.request.url.indexOf("m") + 1)
-	if (origin === ("https://api.github.com"||"https://github.com")) {
-		var url = encodeURIComponent(event.request.url);
-		event.request.url = "https://api.allorigins.win/get?url=" + url;
-		console.log(event.request.url);
-	}
 	event.respondWith(
-		console.log(event)
-		console.log(event.request.url)
-		var origin = event.request.url.slice(0, event.request.url.indexOf("m") + 1)
-		if (origin === ("https://api.github.com"||"https://github.com")) {
-			var url = encodeURIComponent(event.request.url)
-			event.request.url = "https://api.allorigins.win/get?url=" + url
-			console.log(event.request.url)
-		}
-		fetch(event.request)
+		check(event.request)
 	);
 });
-
+function check(request) {
+	console.log(request.url);
+	var origin = request.url.slice(0, request.url.indexOf("m") + 1)
+	if (origin === ("https://api.github.com" || "https://github.com")) {
+		var url = encodeURIComponent(request.url);
+		request.url = "https://api.allorigins.win/get?url=" + url;
+		console.log(request.url);
+	}
+	fetch(request);
+}
