922880dd4da7764a00d7f6320029bd1937dd1d5a
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-07-20 10:01:59 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,14 @@ self.addEventListener('fetch', function(event) {
 		console.log(event.request.url);
 	}
 	event.respondWith(
+		console.log(event)
+		console.log(event.request.url)
+		var origin = event.request.url.slice(0, event.request.url.indexOf("m") + 1)
+		if (origin === ("https://api.github.com"||"https://github.com")) {
+			var url = encodeURIComponent(event.request.url)
+			event.request.url = "https://api.allorigins.win/get?url=" + url
+			console.log(event.request.url)
+		}
 		fetch(event.request)
 	);
 });
