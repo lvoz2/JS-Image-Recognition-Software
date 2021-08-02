6251a89e310d32ede539d083d65b263b314c5409
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2021-08-02 11:23:51 +1000

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -2,17 +2,17 @@ function createscript1() {
 	var script1 = document.createElement('script')
 	script1.src = './js/number_recognition.js'
 	script1.onload = function(){createscript2()}
-	document.head.append(script1)
+	document.head.appendChild(script1)
 }
 function createscript2() {
 	var script2 = document.createElement('script')
 	script2.src = './js/external_code.js'
 	script2.onload = function(){createscript3()}
-	document.head.append(script2)
+	document.head.appendChild(script2)
 }
 function createscript3() {
 	var script3 = document.createElement('script')
 	script3.src = './js/values.js'
-	document.head.append(script3)
+	document.head.appendChild(script3)
 }
 createscript1()
