ac9c5bede4dd31a2f1ffd85d47a4a49d2a7d5078
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-11-09 08:24:00 +1100

    Update stuff.js

--- js/stuff.js
+++ js/stuff.js
@@ -17,7 +17,7 @@ window.windowAuth = function(callback, param) {
 		var code = url.slice(codelocation + 1);
 		scope.popup.close()
 		var ghendpoint = 'https://github.com/login/oauth/access_token?client_id=7415eb3be51e7222a91c&client_secret=f43a1da2796648bb8f8d98a166ff3278d7843624&code=' + code;
-		$.get("https://keep-header-proxy.herokuapp.com/" + ghendpoint, function (access_token) {
+		$.get(ghendpoint, function (access_token) {
 			var token_loc_start = access_token.indexOf("=");
 			var token_loc_end = access_token.indexOf("&");
 			var token = access_token.slice(token_loc_start + 1, token_loc_end);
